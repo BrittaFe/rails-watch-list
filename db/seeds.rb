f08886708960683466af14d2de43f1ab22ed3fec
Britta Felzmann <brittafelzmann97@gmail.com>
2021-05-21 11:25:49 +0200

    Update gemfile.lock with downgraded bundler (for heroku)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+
+file = URI.open(url).read
+
+movies = JSON.parse(file)
+
+movies['results'].each do |movie|
+  title = movie['title']
+  overview = movie['overview']
+  poster_url = "https://image.tmdb.org/t/p/original/#{movie['poster_path']}"
+  rating = movie['vote_average']
+  Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
+end
